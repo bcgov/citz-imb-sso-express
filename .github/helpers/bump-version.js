2ffd313e4a93af6a07758ba2c8c304a840741127
BradyMitch <braden.jr.mitch@gmail.com>
2023-11-06 19:00:26 -0800

    Fixed import

--- .github/helpers/bump-version.js
+++ .github/helpers/bump-version.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 // Parse and validate version numbers
 const parseVersion = (version) => {
