bdacfb0218e5caabec3a09ae91b9e2f2c28c7891
BradyMitch <braden.jr.mitch@gmail.com>
2023-12-19 11:59:22 -0800

    Added hasRole function

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,4 +1,4 @@
-import { KeycloakUser } from "../types";
+import { HasRoleOptions, KeycloakUser } from "../types";
 import { parseJWT } from "./jwt";
 
 // Gets user information from parsing an access token JWT.
@@ -17,3 +17,30 @@ export const hasAtLeastOneRole = (
   userRoles: string[],
   requiredRoles: string[]
 ) => requiredRoles.some((role) => userRoles.includes(role));
+
+// Return true if the user has the specified roles.
+export const hasRole = (
+  user: KeycloakUser,
+  roles: string[],
+  options?: HasRoleOptions
+) => {
+  const userRoles = user?.client_roles;
+
+  // Ensure proper use of function.
+  if (
+    !roles ||
+    !Array.isArray(roles) ||
+    !roles.every((item) => typeof item === "string")
+  )
+    throw new Error(
+      "Error in hasRole function of `citz-imb-kc-express`. Pass roles as an array of strings."
+    );
+
+  // Return false because user does not have any roles
+  if (!userRoles) return false;
+
+  // User must have all roles in roles array unless requireAllRoles === false
+  if (options && options?.requireAllRoles === false)
+    return hasAtLeastOneRole(userRoles, roles);
+  else return hasAllRoles(userRoles, roles);
+};
